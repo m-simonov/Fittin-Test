a820b275e7410b213f92cbe5c51dd1ddeb32d1b6
simonovmaksim <simonov.maksim13@yandex.ru>
2022-07-20 12:38:23 +0300

    First commit

---
+++ api/products/management/commands/parse_feed.py
@@ -0,0 +1,11 @@
+import os
+import xml.etree.ElementTree as ET
+from os.path import dirname, join
+
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
